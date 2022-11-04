76c06d5687269993a2059a7104733840ee7a1560
John Till <john.till@cmrsurgical.com>
2022-11-04 17:18:14 +0000

    Add sphere

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ mod canvas;
 mod matrix;
 mod transformations;
 mod rays;
+mod spheres;
 
 pub use crate::tuple::*;
 pub use crate::canvas::*;
 pub use crate::matrix::*;
 pub use crate::transformations::*;
 pub use crate::rays::*;
+pub use crate::spheres::*;
--- src/rays.rs
+++ src/rays.rs
@@ -1,4 +1,5 @@
 use crate::tuple::*;
+use crate::spheres::*;
 
 pub struct Ray {
     pub origin: Tuple,
@@ -9,4 +10,9 @@ impl Ray {
     pub fn position(&self, distance: f32) -> Tuple {
         self.origin + self.direction * distance
     }
+	
+	pub fn intersect(&self, sphere: Sphere) -> Vec<f32> {
+		let mut intersecting_distances : Vec<f32> = vec![];
+		return intersecting_distances; //DO THIS
+	}
 }
